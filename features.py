391b29bcb6d4f5e03fe2938b198991bd9866d14b
nchortek <nathanchortek@gmail.com>
2018-12-11 16:23:39 -0600

    Update features.py
    in the process of setting up a tfrecord dataset to train the model on

--- features.py
+++ features.py
@@ -12,22 +12,43 @@ feature = {'label': tf.FixedLenFeature([], tf.int64),
         'label_normal': tf.FixedLenFeature([], tf.int64),
         'image': tf.FixedLenFeature([], tf.string)}
 
-
+"""
 # Pulls a record user TFRecordReader.read, and outputs a lab image with 0-5 label, and 0-1 label
-# def load_tfrecord():
-#     _, tfrecord = reader.read(filename_queue)
-#     # Decode the record read by the reader
-#     features = tf.parse_single_example(tfrecord, features=feature)
-#     # Convert the image data from string back to the numbers
-#     image = tf.decode_raw(features['image'], tf.uint8)
-#     # image = tf.image.decode_png(features['image'],channels=1)
-#     label = features['label']
-#     label_normal = features['label_normal']
-#     image = tf.reshape(image, [299, 299, 1])
-#     print(np.array(image))
-#     image_lab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
-#     return (image_lab, label, label_normal)
+ def _parse_(serialized_img):
+     # I dont think this reader is needed anymore
+     #_, tfrecord = reader.read(filename_queue)
+     # Decode the record read by the reader
+     features = tf.parse_single_example(serialized_img, features=feature)
+     # Convert the image data from string back to the numbers
+     image = tf.decode_raw(features['image'], tf.uint8)
+     # image = tf.image.decode_png(features['image'],channels=1)
+     label = features['label']
+     label_normal = features['label_normal']
+     image = tf.reshape(image, [299, 299, 1])
+     print(np.array(image))
+     image_lab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
+     # unsure if this return value is actually what we want
+     return (image_lab, label, label_normal)
 
+
+def process_tfrecords(batch_size=32):
+    tfrecord_dataset = tf.data.TFRecordDataset(filenames)
+    tfrecord_dataset = tfrecord_dataset.map(lambda x:_parse_(x)).shuffle(True) \
+                            .batch(batch_size)
+    tfrecord_iterator = tfrecord_dataset.make_one_shot_iterator()
+    return tfrecord_iterator.get_next()
+
+
+# NOTE: this function needs to be tweeked for whatever model/parameters we want
+def train_model():
+    # defining the type of features columns to be used on model.
+    feature_column = [tf.feature_column.numeric_column(key='image',shape=(784,))]
+
+    # defining the model
+    model = tf.estimator.DNNClassifier([100,100],n_classes=10,feature_columns=feature_column)
+    model.train(lambda:process_tfrecords(32),steps=200)
+    return model
+"""
 
 # Given a image, returns a dictionary with keys of intensities, and values of the number of occurrences of that intensity
 def intensity_frequency(image):
