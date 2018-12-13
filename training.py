e138a96ea2018a77cb6152171f5e4f3c2f7287b9
nchortek <nathanchortek@gmail.com>
2018-12-13 15:16:12 -0600

    Update training.py
    Binary model is done, should probably switch to a categorical classifier though, because our model is super overfitted

--- training.py
+++ training.py
@@ -14,9 +14,6 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import model_from_json
-
 
 
 img_rows = 300
@@ -27,13 +24,8 @@ image_dir = r"skin-lesions\images"
 
 
 lesion_type_dict = {
-    'nv': 'Malignant',
-    'mel': 'Malignant',
+    'mal': 'Malignant',
     'bkl': 'Benign',
-    'bcc': 'Malignant',
-    'akiec': 'Malignant',
-    'vasc': 'Malignant',
-    'df': 'Malignant'
 }
 
 
@@ -43,10 +35,10 @@ def load_data():
 
     #print(imageid_path_dict)
     tile_df = pd.read_csv(metadata_path)
+    tile_df['dx'] = np.where(tile_df['dx'] != 'bkl', 'mal', tile_df['dx'])
     tile_df['path'] = tile_df['image_id'].map(imageid_path_dict.get)
     tile_df['cell_type'] = tile_df['dx'].map(lesion_type_dict.get)
     tile_df['cell_type_idx'] = pd.Categorical(tile_df['cell_type']).codes
-    tile_df['dx'] = np.where(tile_df['dx'] != 'bkl', 'mal', tile_df['dx'])
     #print(tile_df.sample(3))
 
     # NOTE: This line is only for speeding up development, it should be removed
@@ -57,6 +49,7 @@ def load_data():
     tile_df['image'] = tile_df['path'].map(lambda x: np.asarray(Image\
         .open(x).resize((img_rows, img_cols))))
 
+    #print(tile_df.sample(1))
     #print(tile_df['image'].map(lambda x: x.shape).value_counts())
     # TODO: we should save tile_df to disk so we dont have to import
     # all the images each time, too
@@ -64,12 +57,12 @@ def load_data():
 
 
 def split_data(tile_df):
-    lab = tile_df.cell_type_idx
+    #lab = tile_df.cell_type_idx'
 
     # split data into test, train, validation
     # the split is as follows: 80% non-test, 20% test
     # --> then the non-test is split 70-30 into training and validation
-    imgs_train, imgs_test, labels_train, labels_test = train_test_split(tile_df, lab, test_size=0.2)
+    imgs_train, imgs_test, labels_train, labels_test = train_test_split(tile_df, tile_df['cell_type_idx'], test_size=0.2)
 
     imgs_train, imgs_val, labels_train, labels_val = train_test_split(imgs_train, labels_train, test_size=0.3)
 
@@ -78,24 +71,16 @@ def split_data(tile_df):
     # One of the kernels for our dataset subtracted out the mean image value and
     # then divided everything by the standard deviation. Idk enough stats to know
     # why it was being done though lol, I pasted it below:
+    imgs_train = np.asarray(imgs_train['image'].tolist())
+    imgs_test = np.asarray(imgs_test['image'].tolist())
+    imgs_val = np.asarray(imgs_val['image'].tolist())
+
     """
-    x_train = np.asarray(x_train_o['image'].tolist())
-    x_test = np.asarray(x_test_o['image'].tolist())
-
-    x_train_mean = np.mean(x_train)
-    x_train_std = np.std(x_train)
-
-    x_test_mean = np.mean(x_test)
-    x_test_std = np.std(x_test)
-
-    x_train = (x_train - x_train_mean)/x_train_std
-    x_test = (x_test - x_test_mean)/x_test_std
+    print("TEST LABS")
+    print(labels_val)
+    print("TEST IMGS")
+    print(imgs_val[0])
     """
-
-    # Perform one-hot encoding on the labels
-    labels_train = to_categorical(labels_train, num_classes = 2)
-    labels_test = to_categorical(labels_test, num_classes = 2)
-    labels_val = to_categorical(labels_val, num_classes = 2)
 
     return imgs_train, labels_train, imgs_val, labels_val, imgs_test, labels_test
 
@@ -105,11 +90,49 @@ def train_model():
     imgs_train, labels_train, imgs_val, labels_val, imgs_test, labels_test = split_data(tile_df)
 
     # TODO: set up a sequential keras model (there's lots of stuff about this online)
+    model = Sequential()
 
-    # TODO: add some convolution layers with MaxPooling2D's & Dropouts inbetween
-    # Everything I've seen ends with a Flatten and a Dense too
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(img_rows, img_cols, img_channels)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    # TODO: train our model & save it to disk so we dont have to always retrain
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(124, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(124, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    print(model.summary())
+
+    model.compile(optimizer=tf.train.AdamOptimizer(), \
+                           loss='binary_crossentropy', \
+                           metrics=['accuracy'])
+
+    batch_size = 32
+    epochs = 12
+
+    history = model.fit(imgs_train, labels_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(imgs_val, labels_val))
+
+    score = model.evaluate(imgs_test, labels_test, verbose=1)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 
 
-#train_model()
+    # TODO: save model to disk so we dont have to always retrain
+
+
+train_model()
